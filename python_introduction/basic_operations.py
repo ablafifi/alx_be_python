b0e2ffba00769cde7a4e1cd7693700ae5a0c6b97
abderrahmane lafifi <lafifiabd@gmail.com>
2024-09-15 18:05:52 +0100

    add file python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(input("enter the first number : "))
+number2 = int(input("entre the second number : "))
+
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {Addition}")
+print(f"Subtraction of {number1} and {number2} is {Subtraction}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication}")
