b4d50cfe37b45970d95a1113f4b63ade3479b672
abderrahmane lafifi <lafifiabd@gmail.com>
2024-09-22 22:47:57 +0100

    Add match_case_calculator file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,16 +2,16 @@ X = int(input("Enter the first number:"))
 Y = int(input("Enter the second number:"))
 
 operation = input("Choose the operation (+, -, *, /):")
-
-if operation == "+":
-    print("The result is " + str(X + Y) + ".")
-elif operation == "-":
-    print("The result is " + str(X - Y) + ".")
-elif operation == "*":
-    print("The result is " + str(X * Y) + ".")
-elif operation == "/":
-    if Y == 0:
-        print("Cannot divide by zero.")
-    else:
-        print("The result is " + str(int(X / Y)) + ".")
+match operation:
+    case"+":
+        print("The result is " + str(X + Y) + ".")
+    case "-":
+        print("The result is " + str(X - Y) + ".")
+    case "*":
+        print("The result is " + str(X * Y) + ".")
+    case "/":
+        if Y == 0:
+            print("Cannot divide by zero.")
+        else:
+            print("The result is " + str(int(X / Y)) + ".")
     
